f3713e254eb0357f4b585285d073af519113da63
Valera Kalinichenko <studiya3@gmail.com>
2023-11-23 21:25:54 +0200

    advantages section

--- src/components/sections/AdvantagesSection.vue
+++ src/components/sections/AdvantagesSection.vue
@@ -1,27 +1,49 @@
 <template>
   <section class="advantages">
     <div class="advantages__wrapper wrapper">
-      <h2 class="advantages__title"></h2>
-      <h3 class="advantages__subtitle"></h3>
-      <p class="advantages__text"></p>
+      <h2 class="advantages__title">Why Decima</h2>
+      <h3 class="advantages__subtitle">Trust your business needs to a true Magento expert team.</h3>
+      <p class="advantages__text">
+        We're a skilled Magento team building digital solutions that unite intelligence and
+        technology
+      </p>
       <ul class="advantages__list">
         <li class="advantages__item">
-          <img class="advantages__item-img" src="" alt="" />
-          <h3 class="advantages__item-title"></h3>
-          <p class="advantages__item-descr"></p>
+          <img
+            class="advantages__item-img"
+            src="src/assets/imgs/advantages-section/slice-1.webp"
+            alt="Magento Commerce logo"
+          />
+          <h3 class="advantages__item-title">Magento Contribution Partner</h3>
+          <p class="advantages__item-descr">
+            Our developers improve Magento 2 code and extend it with bew functionalities
+          </p>
         </li>
         <li class="advantages__item">
-          <img class="advantages__item-img" src="" alt="" />
-          <h3 class="advantages__item-title"></h3>
-          <p class="advantages__item-descr"></p>
+          <img
+            class="advantages__item-img"
+            src="src/assets/imgs/advantages-section/slice-2.webp"
+            alt="An event speaker photo"
+          />
+          <h3 class="advantages__item-title">Magento Events Speaker</h3>
+          <p class="advantages__item-descr">
+            You always can find us on the stage at all Magento events, where we share success
+            stories
+          </p>
         </li>
         <li class="advantages__item">
-          <img class="advantages__item-img" src="" alt="" />
-          <h3 class="advantages__item-title"></h3>
-          <p class="advantages__item-descr"></p>
+          <img
+            class="advantages__item-img"
+            src="src/assets/imgs/advantages-section/slice-3.webp"
+            alt="Magento top pioneers image"
+          />
+          <h3 class="advantages__item-title">Magento Top Pioneers</h3>
+          <p class="advantages__item-descr">
+            We were at the origins of the Magento and 60 % of our team was top execitives in Magento
+          </p>
         </li>
       </ul>
-      <button class="advantages__btn btn btn-orange" type="button"></button>
+      <button class="advantages__btn btn btn-orange" type="button">Contact us for advice</button>
     </div>
   </section>
 </template>
