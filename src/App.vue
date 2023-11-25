6c10e0793d3cc35c2d9b5721d056aa47a597ed65
Valera Kalinichenko <studiya3@gmail.com>
2023-11-25 17:20:39 +0200

    footer

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,12 @@
 <template>
   <TheHeader />
   <router-view />
-  <footer></footer>
+  <TheFooter />
 </template>
 
 <script setup>
 import TheHeader from './components/layouts/TheHeader.vue'
+import TheFooter from './components/layouts/TheFooter.vue'
 </script>
 
 <style lang="scss"></style>
---
+++ src/components/layouts/TheFooter.vue
@@ -0,0 +1,84 @@
+<template>
+  <footer class="footer">
+    <div class="footer__wrapper wrapper">
+      <div class="footer__top">
+        <address class="footer__address">
+          <div class="footer__soc">
+            <h3>Follow us</h3>
+            <ul class="footer__soc-list">
+              <li class="footer__soc-item">
+                <a href="#">
+                  <svg class="footer__soc-fb">
+                    <use href="src/assets/imgs/footer/soc-icons.svg#icon-facebook"></use></svg
+                ></a>
+              </li>
+              <li class="footer__soc-item">
+                <a href="#"
+                  ><svg class="footer__soc-inst">
+                    <use href="src/assets/imgs/footer/soc-icons.svg#icon-instagram"></use></svg
+                ></a>
+              </li>
+              <li class="footer__soc-item">
+                <a href="#"
+                  ><svg class="footer__soc-tw">
+                    <use href="src/assets/imgs/footer/soc-icons.svg#icon-twitter"></use></svg
+                ></a>
+              </li>
+              <li class="footer__soc-item">
+                <a href="#"
+                  ><svg class="footer__soc-link">
+                    <use href="src/assets/imgs/footer/soc-icons.svg#icon-linkedin"></use></svg
+                ></a>
+              </li>
+            </ul>
+          </div>
+          <div class="footer__contacts">
+            <h3>Contact us</h3>
+            <div class="footer__contacts-inner">
+              <ul class="footer__contacts-tels">
+                <li class="footer__contacts-item">
+                  <span>US:</span> <a href="tel:+1 267 460 04 88">+1 267 460 04 88</a>
+                </li>
+                <li class="footer__contacts-item">
+                  <span>Europe:</span> <a href="tel:+372 712 33 30">+372 712 33 30</a>
+                </li>
+                <li class="footer__contacts-item">
+                  <span>UK:</span> <a href="tel:+44 208 123 0701">+44 208 123 0701</a>
+                </li>
+              </ul>
+              <ul class="footer__contacts-mails">
+                <li class="footer__contacts-item">
+                  <a href="mailto:services@decimadigital.com">services@decimadigital.com</a>
+                </li>
+                <li class="footer__contacts-item">
+                  <a href="mailto:marketing@decimadigital.com">marketing@decimadigital.com</a>
+                </li>
+              </ul>
+            </div>
+          </div>
+          <div class="footer__places">
+            <h3>Decima Digital Inc</h3>
+            <ul class="footer__places-list">
+              <li class="footer__places-item">
+                <h4>USA</h4>
+                <a href="https://maps.app.goo.gl/EAakBPxQbDgau2xA6"
+                  >42 Broadway, Suite 12-484, New York NY 10004, United States</a
+                >
+              </li>
+              <li class="footer__places-item">
+                <h4>Europe</h4>
+                <a href="https://maps.app.goo.gl/Qs6jMCM7BDLJUMQ56"
+                  >Estonia pst. 5 - 309B 10143 Tallinn - Estonia</a
+                >
+              </li>
+            </ul>
+          </div>
+        </address>
+      </div>
+      <div class="footer__bottom">
+        <p class="footer__copyright">© Copyright 2020. Privacy policy.</p>
+        <p class="footer__license">License Agreement</p>
+      </div>
+    </div>
+  </footer>
+</template>
